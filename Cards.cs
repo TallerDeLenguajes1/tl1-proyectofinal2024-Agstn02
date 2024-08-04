550e20c1ba9eedc44794f2dda2673942cb75bf0d
Agustin <agochoc1@gmail.com>
2024-08-04 03:33:30 -0300

    terminado lógica de HandValue

--- Cards.cs
+++ Cards.cs
@@ -17,17 +17,17 @@ namespace Cards
         CartaAlta,
         Par,
         DoblePar,
+        Trio,
         Escalera,
         Color,
         FullHouse,
-        Trio,
-        EscaleraColor,
         Poquer,
+        EscaleraColor,
         EscaleraReal
     }
     //Uso la clase PokerStraights para poder deserializar el resultado en json desde el archivo en la carpeta assets.
-    public class StraightsValues{
-        public List<int[]> PokerStraight {get ; set;}
+    public class PokerStraight{
+        public List<int[]> PokerStraights {get ; set;}
     } 
     //Clase HAND - Maneja las cartas que se reparten y las cartas de mesa. Identifica la jugada y permite modificar las cartas.
     public class Hand(List<Card> poket)
@@ -42,13 +42,13 @@ namespace Cards
             _hand.AddRange(tableCards);
         }
         //CountSuits() devuelve un diccionario con la cantidad de cartas de cada suit.
-        private Dictionary<string, int> CountSuits(List<Card> hand){
+        private Dictionary<string, int> CountSuits(){
             Dictionary<string , int> suits = new();
             suits.Add("SPADES",0);
             suits.Add("HEARTS",0);
             suits.Add("CLUBS",0);
             suits.Add("DIAMONDS",0);
-            foreach (Card item in hand)
+            foreach (Card item in _hand)
             {
                 switch (item.Suit)
                 {
@@ -62,11 +62,21 @@ namespace Cards
                     suits["CLUBS"] += 1;
                     break;
                     case "DIAMONDS":
-                    suits["diamonds"] += 1;
+                    suits["DIAMONDS"] += 1;
                     break;
                 }
             }
             return suits;
+        }
+        private Dictionary<string, int> CountValues(){
+            Dictionary<string, int> values = [];
+            foreach (var item in _hand)
+            {
+                if(!values.TryAdd(item.Value, 1)){//TryAdd intenta agregar el elemento al diccionario, si el key ya existe, devuelve false;
+                    values[item.Value] += 1;
+                }
+            }
+            return values;
         }
         private string NormalizeHand(){
             //Creo una Lista de los valores parseados a su valor entero.
@@ -75,10 +85,10 @@ namespace Cards
             {
                 ParsedValues.Add( _hand[i].Value switch
                     {
-                        "J" => 11,
-                        "Q" => 12,
-                        "K" => 13,
-                        "A" => 14,
+                        "JACK" => 11,
+                        "QUEEN" => 12,
+                        "KING" => 13,
+                        "ACE" => 14,
                         _ => int.Parse(_hand[i].Value),
                     });
             }
@@ -97,9 +107,9 @@ namespace Cards
             //Para eso uso el archivo straight.json
             string StraightsFile = File.ReadAllText("assets/straight.json");
             //Deserializo en la clase StraightsValues
-            var straigths = JsonSerializer.Deserialize<StraightsValues>(StraightsFile);
+            var straigths = JsonSerializer.Deserialize<PokerStraight>(StraightsFile);
             //Itero en la lista de arrays de los valores para buscar alguna coincidencia:
-            foreach (var item in straigths.PokerStraight)
+            foreach (var item in straigths.PokerStraights)
             {
                 if(StringValues.Contains(item.ToString())){//Si para el string(obtenido de un int[]) existe una coincidencia entonces es una escalera.
                     return true;
@@ -118,26 +128,81 @@ namespace Cards
             }
             return false;
         }
+        
         public void DefineValue(){
-            var suits = CountSuits(_hand);
+            var suits = CountSuits();
+            var ValuesDic = CountValues();
             bool IsFlushed = suits.ContainsValue(5) | suits.ContainsValue(6) | suits.ContainsValue(7);
-            if(IsFlushed){ // Posible escalera real, escalera de color o color.
-                if(!IsStraight()){
-                    value = HandValue.Color;
-                }
-                else if(!IsRoyal())
+            if(IsStraight()){ // Posible escalera real, escalera de color o escalera
+                if(IsFlushed)
                 {
-                    value = HandValue.EscaleraColor;
+                    if (IsRoyal())
+                    {
+                        value = HandValue.EscaleraColor;
+                    }
+                    else
+                    {
+                        value = HandValue.Color;
+                    }
                 }
                 else
                 {
-                    value = HandValue.EscaleraReal;
+                    value = HandValue.Escalera;
                 }
             }
-            else 
+            else //Resto de jugadas.
             {
-                //Puede ser cualquier jugada que no involucre los suits, y amerita evaluar los valores de las cartas.
-
+                //Puede ser cualquier jugada que no involucre los suits excepto color, y amerita evaluar los valores de las cartas.
+                if(ValuesDic.ContainsValue(4))//Hay un poker
+                {
+                    value = HandValue.Poquer;
+                }
+                else if (ValuesDic.ContainsValue(3)) //Hay full o trio.(o color)  
+                {
+                    if(ValuesDic.ContainsValue(2)){ //Si ademas de contener 3, contiene 2 => Es FullHouse.
+                        value = HandValue.FullHouse;
+                    }
+                    else if (IsFlushed) //Es color
+                    {
+                        value = HandValue.Color;
+                    }
+                    else // Es trio o pierna.
+                    {
+                        value = HandValue.Trio;
+                    }
+                }
+                else if(ValuesDic.ContainsValue(2))//Hay par o doble par. (o color).
+                {
+                    int pairs = 0;
+                    foreach (var value in ValuesDic.Values) //Cuento la cantidad de Pares que hay. Si hay dos o mas entoces hay doble par.
+                    {
+                        if(value == 2 ){
+                            pairs++;
+                        }
+                    }
+                    if(pairs >= 2) //Hay doble par
+                    {
+                        value = HandValue.DoblePar;
+                    }
+                    else//Hay par
+                    {
+                        value = HandValue.Par;
+                    }
+                    if (IsFlushed)//Estoy odiando el color. Debo chequear igual porque podria existir un color
+                    {
+                        value = HandValue.Color;
+                    }
+                }
+                else//Hay carta alta o color.
+                {
+                    if(IsFlushed){//Hay color 
+                        value = HandValue.Color;
+                    }
+                    else // Hay carta alta.
+                    {
+                        value = HandValue.CartaAlta;
+                    }
+                }
             }
 
         }
@@ -145,8 +210,9 @@ namespace Cards
         public void Show(){
             foreach (var item in _hand)
             {
-                Console.WriteLine(item.Code);                
+                Console.WriteLine(item.Code);
             }
+            Console.WriteLine(value);
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -12,9 +12,14 @@ personajes[1].Hand = new Hand(newDeck.DealPoket());
 personajes[0].MostrarStats();
 personajes[1].MostrarStats();
 
-personajes[0].Hand.GetCards(newDeck.DealTable());
-personajes[1].Hand.GetCards(newDeck.DealTable());
+var table = newDeck.DealTable();
+personajes[0].Hand.GetCards(table);
+personajes[1].Hand.GetCards(table);
 
 personajes[0].Hand.DefineValue();
 personajes[1].Hand.DefineValue();
 
+personajes[0].MostrarStats();
+personajes[1].MostrarStats();
+
+Console.ReadLine();
